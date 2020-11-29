06da851b133a7ab6100ec3a18f96dd0bf0e6c2b0
gen <19019230@brookes.ac.uk>
2020-11-29 19:10:14 +0000

    i done my task?

---
+++ app/src/main/java/project/crimewatch/ui/EmergencyContact.java
@@ -0,0 +1,22 @@
+package project.crimewatch.ui;
+
+public class EmergencyContact {
+
+    private String WebReport;
+
+
+    public EmergencyContact(String WebReport) {
+        this.WebReport = WebReport;
+    }
+
+
+
+
+    public String getWebReport() {
+        return WebReport;
+    }
+
+    public void setWebReport(String webReport) {
+        WebReport = webReport;
+    }
+}
